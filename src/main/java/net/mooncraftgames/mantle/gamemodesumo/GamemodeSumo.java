dbb2d2371342f0f8a253459f4c9f6c0d652019d8
CG360 <CloudGamer360@users.noreply.github.com>
2020-06-26 10:46:15 +0100

    Registered game and slapper kit

--- src/main/java/net/mooncraftgames/mantle/gamemodesumo/GamemodeSumo.java
+++ src/main/java/net/mooncraftgames/mantle/gamemodesumo/GamemodeSumo.java
@@ -2,7 +2,15 @@ package net.mooncraftgames.mantle.gamemodesumo;
 
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.plugin.PluginLogger;
+import net.mooncraftgames.mantle.gamemodesumo.game.GameBehaviorSumo;
+import net.mooncraftgames.mantle.gamemodesumo.kits.KitSlapper;
 import net.mooncraftgames.mantle.newgamesapi.NewGamesAPI1;
+import net.mooncraftgames.mantle.newgamesapi.game.GameHandler;
+import net.mooncraftgames.mantle.newgamesapi.game.GameID;
+import net.mooncraftgames.mantle.newgamesapi.game.GameProperties;
+import net.mooncraftgames.mantle.newgamesapi.kits.KitGroup;
+import net.mooncraftgames.mantle.newgamesapi.registry.GameRegistry;
+import net.mooncraftgames.mantle.newgamesapi.registry.KitRegistry;
 
 public class GamemodeSumo extends PluginBase {
 
@@ -12,6 +20,19 @@ public class GamemodeSumo extends PluginBase {
     public void onEnable() {
         if(!getServer().getPluginManager().isPluginEnabled(NewGamesAPI1.get())) return;
         gamemodeSumo = this;
+
+        KitRegistry.get().registerKitGroup(new KitGroup("sumo", new KitSlapper()));
+
+        GameProperties sumoProperties = new GameProperties(GameHandler.AutomaticWinPolicy.MANUAL_CALLS_ONLY)
+                .setCanPlayersMoveDuringCountdown(false)
+                .setCanWorldBeManipulated(false)
+                .setDefaultCountdownLength(10)
+                .setMinimumPlayers(4)
+                .setGuidelinePlayers(6)
+                .setMaximumPlayers(16);
+        GameID sumoID = new GameID("sumo", "sumo", "Sumo", "Slap players off the platform in a multi-rounded gamemode! The last standing wins each round!", "sumo", new String[]{"sumo"}, 1, sumoProperties, GameBehaviorSumo.class);
+
+        GameRegistry.get().registerGame(sumoID);
     }
 
     public static GamemodeSumo get(){ return gamemodeSumo; }
---
+++ src/main/java/net/mooncraftgames/mantle/gamemodesumo/game/GameBehaviorSumo.java
@@ -0,0 +1,16 @@
+package net.mooncraftgames.mantle.gamemodesumo.game;
+
+import net.mooncraftgames.mantle.newgamesapi.game.GameBehavior;
+
+public class GameBehaviorSumo extends GameBehavior {
+
+    @Override
+    public void onInitialCountdownEnd() {
+
+    }
+
+    @Override
+    public void registerGameSchedulerTasks() {
+
+    }
+}
